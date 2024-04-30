f5079f1eb58eaf6bf40a0d78878d1856e241d308
Lazah <pentti.laitinen@gmail.com>
2024-04-30 18:56:08 +0300

    finished program

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main ():
     file_contents = get_content("books/frankenstein.txt")
     word_count = count_words(file_contents)
-    print(word_count)
     chars = count_chars(file_contents)
-    print(chars)
+    chars.sort(reverse=True,key=sort_on)
+    print_report(word_count,chars,"books/frankenstein.txt")
 
 def count_words (long_string:str):
     arr = long_string.split()
@@ -18,9 +18,23 @@ def get_content (path:str):
 def count_chars (long_sting:str):
     lower_case_str = long_sting.lower()
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    chars = {}
+    result_list = []
     for letter in alphabet:
-        chars[letter] = lower_case_str.count(letter)
-    return chars
+        char = {}
+        char["count"] = lower_case_str.count(letter)
+        char["char"] = letter
+        result_list.append(char)
+    return result_list
+
+def sort_on(dict):
+    return dict["count"]
+
+def print_report(total_count:int,char_list:list,book_path:str):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{total_count} words found in the document\n")
+    for char_dict in char_list:
+        print(f"The '{char_dict["char"]}' character was found {char_dict["count"]} times")
+
+    print("--- End report ---")
 
 main()
